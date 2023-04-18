3324807aa0c1e4c301808b10a9d78c114318423d
Dante Scanline <dantescanline@posteo.net>
2023-04-19 00:28:14 +0200

    add terminal sizes to make floating rectangle of colors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::time::Duration;
+
 use colored::Color;
 use colored::Colorize;
 use rand::random;
 
+extern crate termion;
+
 /// This is the main function... obviously
 fn main() {
+    // todo: use some clever enum to array thingy to do this automatically
     let colors = [
         Color::Black,
         Color::Red,
@@ -25,16 +30,30 @@ fn main() {
 
     let mut i = 3;
 
-    for _x in 1..20002 {
-        let this_color = colors[i];
-        let some_text = "#".color(this_color);
-        print!("{}", some_text);
+    print!("{}", termion::clear::All);
 
-        // Do some random here
-        if random::<f32>() > 0.8 {
-            i += 1;
+    let size = termion::terminal_size().unwrap();
+
+    loop {
+        for y in 4..(size.1 - 2) {
+            for x in 6..(size.0 - 3) {
+                let this_color = colors[i];
+
+                let chars = ["#"];
+
+                let index = (random::<f32>() * (chars.len() as f32)) as usize;
+                let char: &str = chars[index];
+                let some_text = char.color(this_color);
+
+                //     // Do some random here
+                if random::<f32>() > 0.2 {
+                    i += 1;
+                }
+
+                i %= colors.len();
+                print!("{}{}", termion::cursor::Goto(x, y), some_text);
+            }
         }
-        i %= colors.len()
+        std::thread::sleep(Duration::from_millis(90));
     }
-    println!()
 }
